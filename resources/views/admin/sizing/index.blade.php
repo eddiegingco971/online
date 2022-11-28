97b8027491dfa9c83ae19a91122f210bf4ee1649
Eddie Gingco <gingcoeddie971@gmail.com>
2022-11-28 09:48:10 +0800

    updated

--- resources/views/admin/sizing/index.blade.php
+++ resources/views/admin/sizing/index.blade.php
@@ -66,14 +66,14 @@
 
             <div class="card elevation-3">
               <div class="card-header">
-                <h3 class="card-title">List of Category</h3>
+                <h3 class="card-title">List of Sizing</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
                 <table id="example1" class="table table-bordered table-striped">
                   <thead>
                   <tr>
-                    <th>Category ID</th>
+                    <th>Category Name</th>
                     <th>Category Image</th>
                     <th>Category Name</th>
                     <th>Action</th>
