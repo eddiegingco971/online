459163952893aa165b324df15f71d54a048a8aae
Eddie Gingco <gingcoeddie971@gmail.com>
2022-11-29 15:09:43 +0800

    updated

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
     public function index(){
-        return view('user.user-checkout.index');
-
+        $users = User::get();
+        return view('user.user-checkout.index', compact('users'));
     }
 }
--- resources/views/livewire/user-checkout.blade.php
+++ resources/views/livewire/user-checkout.blade.php
@@ -1,6 +1,6 @@
 <div>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-    <div class="py-3 py-md-4 checkout">
+    {{-- <div class="py-3 py-md-4 checkout">
         <div class="container">
             <h4>Checkout</h4>
             <hr>
@@ -13,9 +13,7 @@
                             <span class="float-end">₱ {{$this->totalProductAmount}}</span>
                         </h4>
                         <hr>
-                        <small>* Items will be delivered in 3 - 5 days.</small>
-                        <br/>
-                        <small>* Tax and other charges are included ?</small>
+                        <small>* Items will be delivered in 10 - 15 minutes.</small>
                     </div>
                 </div>
                 <div class="col-md-12">
@@ -44,6 +42,7 @@
                                 <div class="col-md-6 mb-3">
                                     <label>Email Address</label>
                                     <input type="email" wire:model="email" class="form-control" placeholder="Enter Email Address" />
+                                    <input type="email"  class="form-control" value="{{auth()->user()->email}}" />
                                     @error('emails')
                                         <small class="text-danger"> {{$message}}</small>
                                     @enderror
@@ -86,5 +85,186 @@
 
             </div>
         </div>
-    </div>
+    </div> --}}
+
+    <div class="content">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <div class="container-fluid">
+            <div class="row mb-2">
+              <div class="col-sm-6">
+                <h1>Checkout</h1>
+              </div>
+              <div class="col-sm-6">
+                {{-- <ol class="breadcrumb float-sm-right">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active">Invoice</li>
+                </ol> --}}
+              </div>
+            </div>
+          </div><!-- /.container-fluid -->
+        </section>
+
+        <section class="content">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-12">
+                {{-- <div class="callout callout-info">
+                  <h5><i class="fas fa-info"></i> Note:</h5>
+                  This page has been enhanced for printing. Click the print button at the bottom of the invoice to test.
+                </div> --}}
+
+
+                <!-- Main content -->
+                <div class="invoice p-3 mb-3">
+                  <!-- title row -->
+                  <div class="row">
+                    <div class="col-12">
+                      <h4>
+                        <i class="fas fa-globe"></i> Mac Kaon FoodHub.
+                        <small class="float-right">Date: {{auth()->user()->created_at}}</small>
+                      </h4>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                  <!-- info row -->
+                  <div class="row invoice-info">
+                    {{-- <div class="col-sm-4 invoice-col">
+                      From
+                      <address>
+                        <strong>Admin, Inc.</strong><br>
+                        795 Folsom Ave, Suite 600<br>
+                        San Francisco, CA 94107<br>
+                        Phone: (804) 123-5432<br>
+                        Email: info@almasaeedstudio.com
+                      </address>
+                    </div> --}}
+                    <!-- /.col -->
+                    <div class="col-sm-4 invoice-col">
+                      To
+                      <address>
+                        <strong>{{auth()->user()->firstname}} {{auth()->user()->lastname}}</strong><br>
+                        {{auth()->user()->address}}<br>
+                        {{auth()->user()->barangay}}<br>
+                        Phone: {{auth()->user()->phone_number}}<br>
+                        Email: {{auth()->user()->email}}
+                      </address>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-4 invoice-col">
+                      <b>Invoice #007612</b><br>
+                      <br>
+                      <b>Order ID:</b> 4F3S8J<br>
+                      <b>Payment Due:</b> 2/22/2014<br>
+                      <b>Account:</b> 968-34567
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                  <!-- /.row -->
+
+                  <!-- Table row -->
+                  {{-- <div class="row">
+                    <div class="col-12 table-responsive">
+                      <table class="table table-striped">
+                        <thead>
+                        <tr>
+                          <th>Qty</th>
+                          <th>Product</th>
+                          <th>Serial #</th>
+                          <th>Description</th>
+                          <th>Subtotal</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Call of Duty</td>
+                          <td>455-981-221</td>
+                          <td>El snort testosterone trophy driving gloves handsome</td>
+                          <td>$64.50</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Need for Speed IV</td>
+                          <td>247-925-726</td>
+                          <td>Wes Anderson umami biodiesel</td>
+                          <td>$50.00</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Monsters DVD</td>
+                          <td>735-845-642</td>
+                          <td>Terry Richardson helvetica tousled street art master</td>
+                          <td>$10.70</td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Grown Ups Blue Ray</td>
+                          <td>422-568-642</td>
+                          <td>Tousled lomo letterpress</td>
+                          <td>$25.99</td>
+                        </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.col -->
+                  </div> --}}
+                  <!-- /.row -->
+
+                  <div class="row">
+                    <!-- accepted payments column -->
+                    <div class="col-6">
+                      <p class="lead">Payment Methods:</p>
+                      <img src="../../dist/img/credit/cod.png" height="40px" width="70px" alt="Gcash" style="border-radius: 5px; border: 1px solid black;">
+                      <img src="../../dist/img/credit/gcash.png" height="40px" width="70px" alt="Gcash" style="border-radius: 5px; border: 1px solid black;">
+
+                      <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
+                       Prepare the exact amount of payment.
+                      </p>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-6">
+                      {{-- <p class="lead">To be pay</p> --}}
+
+                      <div class="table-responsive">
+                        <table class="table">
+                          <tr>
+                            <th style="width:50%">Subtotal:</th>
+                            <td>₱ {{$this->totalProductAmount}}</td>
+                          </tr>
+                          <tr>
+                            <th>Delivery Fee:</th>
+                            <td>₱ </td>
+                          </tr>
+                          <tr>
+                            <th>Total:</th>
+                            <td>₱ 265.24</td>
+                          </tr>
+                        </table>
+                      </div>
+                    </div>
+                    <!-- /.col -->
+                  </div>
+                  <!-- /.row -->
+
+                  <!-- this row will not appear when printing -->
+                  <div class="row no-print">
+                    <div class="col-12">
+                      <button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Cash On Delivery
+                      </button>
+                      <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
+                        <i class="fas fa-money-bill"></i> Scan Gcash QR code
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.invoice -->
+              </div><!-- /.col -->
+            </div><!-- /.row -->
+          </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+      </div>
+
+
 </div>
